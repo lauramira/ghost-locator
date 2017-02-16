8ee0f7f2043c4e4080f6acfcb8b8bc74f85ebe1e
albert.denova <albert.denova@king.com>
2017-02-16 14:46:05 +0100

    Create fragments and setup basic bottom navigation view

--- app/src/main/java/com/lasalle/second/part/ghostlocator/view/activity/MainActivity.java
+++ app/src/main/java/com/lasalle/second/part/ghostlocator/view/activity/MainActivity.java
@@ -1,15 +1,28 @@
 package com.lasalle.second.part.ghostlocator.view.activity;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.lasalle.second.part.ghostlocator.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.main_activity_bottom_navigation_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
---
+++ app/src/main/java/com/lasalle/second/part/ghostlocator/view/fragment/SearchFragment.java
@@ -0,0 +1,29 @@
+package com.lasalle.second.part.ghostlocator.view.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lasalle.second.part.ghostlocator.R;
+
+public class SearchFragment extends Fragment {
+
+    public static SearchFragment newInstance() {
+        SearchFragment fragment = new SearchFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+}
