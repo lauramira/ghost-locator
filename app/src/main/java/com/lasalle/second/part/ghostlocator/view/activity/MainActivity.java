adde995ac5bee8fdd53bb425f55a80c46e8d152b
albert.denova <albert.denova@king.com>
2017-02-16 14:29:11 +0100

    Project setup

---
+++ app/src/main/java/com/lasalle/second/part/ghostlocator/view/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lasalle.second.part.ghostlocator.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lasalle.second.part.ghostlocator.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
